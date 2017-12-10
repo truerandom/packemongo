cb21915a9e345048e24151fb625d6fcf3b05e3ce
chaos <chaos@localhost.localdomain>
2017-12-10 18:07:05 -0500

    Ya funciona al enviar las imagenes falta matar el socket cuando se cierran sesiones

--- Server.java
+++ Server.java
@@ -1,199 +1,150 @@
-// File Name GreetingClient.java
+// File Name GreetingServer.java
+//https://stackoverflow.com/questions/33087890/multithreading-with-client-server-program
 import java.net.*;
 import java.io.*;
 import java.util.Arrays;
-import java.util.Scanner;
-import java.util.*;
-import java.awt.Desktop;
+import java.nio.file.Files;
+import java.io.File;
 import javax.imageio.ImageIO;
-import javax.swing.*;
 import java.awt.image.BufferedImage;
-public class Client {
-	// Parse response debe regresar un entero indicando si a continuacion debe haber entrada del usuario
-	// 1 entrada del usuario 0 eoc
-	// El caso default es cuando se recibe la imagen
-	// debo tener un buffer para ir guardando lo de la imagen
-	static List myBytes = new ArrayList<Byte>();
-	public static int parseResponse(DataInputStream in,DataOutputStream out){
-		try{
-			//System.out.println("Entre a parseResponse");
-			byte[] data = new byte[1024];
-			//Cuantos datos envio el server
-			int count = in.read(data);
-			int idpok=-1;
-			int intentos=-1;
-			int imgsize = -1;
-			//System.out.println("Count es "+count);
-			/*
-			for(int i=0;i<count;i++)	
-				System.out.print(data[i]+" ");
-			*/
-			if(count !=-1){
-				switch(data[0]){
-					case 20:
-						idpok= data[1];
-						System.out.println("Capturar pokemon? id "+idpok);
-						return 1;
-					case 21:
-						idpok = data[1];
-						intentos = data[2];
-						System.out.println("Reintentar id "+idpok+" intentos "+intentos);
-						return 1;
-					case 22:
-						idpok = data[1];
-						imgsize = data[2];
-						System.out.println("Pokemon capturado "+idpok);
-						System.out.println("Img size: "+imgsize);
-						//Aqui recibo los siguientes paquetes
-						getImage(in);
-						return 0;
-					case 23:
-						System.out.println("Intentos agotados");
-						terminaSesion(out);
-						return 0;
-					case 32:
-						System.out.println("Terminando sesion");
-						System.exit(1);
-						return 0;
-					//Si no es algun caso anterior lo pego a buffer de img
-					default:
-						System.out.println("Entre a def no deberia");
-						System.exit(1);
-						return 0;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Desktop;
+// xdg-open abre la imagen con el manejador default
+public class Server extends Thread {
+   private ServerSocket serverSocket;
+   Socket server;
+   byte intentos;
+   int estado;
+   public Server(Socket server,int port) throws IOException {
+      //serverSocket = new ServerSocket(port);
+      //serverSocket.setSoTimeout(18000);
+      System.out.println("Entre al constructor con servers");
+      this.server = server;
+      System.out.println(server);
+      //intentos de captura
+      intentos = 3;
+      //estado
+      estado = 0;
+   }
+	
+   public void run() {
+         try {
+            //System.out.println("Waiting for client on port " + serverSocket.getLocalPort() + "...");
+            //Socket server = serverSocket.accept();
+            
+            System.out.println("Just connected to " + this.server.getRemoteSocketAddress());
+            DataInputStream in = new DataInputStream(server.getInputStream());
+            DataOutputStream out = new DataOutputStream(server.getOutputStream());
+            int c = 0;
+            byte res=0;
+			byte pokid=-1;
+			byte[] arr;
+			boolean continuar=true;
+            while(!this.isInterrupted() && c < 50) {
+				System.out.println("Processing with "+server);
+				byte[] recibidos = new byte[3];
+				int numbytes = in.read(recibidos);
+				//System.out.println(in.available());
+				System.out.println("Numero de bytes recibidos "+numbytes);
+				c++;
+				if(numbytes!=-1){
+					System.out.println("Debo procesar");
+					System.out.println("Recibido "+Arrays.toString(recibidos));
+					int opcion = recibidos[0];
+					switch(opcion){
+						// Se recibio un intento de conexion
+						case 10:
+							System.out.println("Recibi inicio de conexion");
+							this.estado = 1;
+							res = 20;
+							pokid = (byte)((Math.random()* 10));
+							arr = new byte[]{res,pokid};
+							System.out.println("Pok id "+pokid);
+							out.write(arr);
+							break;
+						// decidio capturar el pokemon
+						case 30:
+							System.out.println("Intentos: "+this.intentos);
+							if(this.intentos > 0){
+								if(this.estado == 1){
+									System.out.println("Puedes capturar");
+									byte resx = (byte)(((Math.random()* 10)) %3);
+									System.out.println("El resultado fue"+resx);
+									//if(resx == 1){ //Chanfle
+									if(resx == 1){
+										System.out.println("Packemon capturado");
+										arr = new byte[]{22,pokid,100};
+										out.write(arr);
+										System.out.println("Enviando img");
+										// Leo la img
+										// Funcion
+										//Chanfleloquitearr = new byte[]{8,8,8,8,8,8,8,8};
+										//out.write(arr);
+										out = new DataOutputStream(server.getOutputStream());
+										System.out.println("pase out");
+										//Robot bot;
+										//bot = new Robot();
+										// exp
+										File fnew=new File("1.jpg");
+										BufferedImage oImage=ImageIO.read(fnew);
+										System.out.println("pase oimage");
+										ImageIO.write(oImage,"JPG",out);	
+										System.out.println("pase write");
+										//exit
+										this.server.close();
+										// finexp
+										/*
+										BufferedImage bimg = bot.createScreenCapture(new Rectangle(0, 0, 200, 100));
+										ImageIO.write(bimg,"JPG",out);
+										System.exit(1);
+										*/
+									}
+									else{
+										arr = new byte[]{21,pokid,this.intentos};
+										intentos--;
+										out.write(arr);
+									}
+								}else{
+									System.out.println("Intento de captura en un edo no valido");
+								}
+							}else{
+								System.out.println("Intentos agotados");
+								// Debo mandar el paquete 23
+								arr = new byte[]{23};
+								out.write(arr);
+							}
+							break;
+						case 31:
+							System.out.println("Terminando la sesion del cliente");
+							arr = new byte[]{32};
+							out.write(arr);
+						case 32:
+							System.out.println("Terminando la sesion del cliente");
+							arr = new byte[]{32};
+							out.write(arr);
+					}
+				}else{
+					System.exit(1);
 				}
-			}else{
-				System.exit(1);
-			}
-			return 0;
-			//System.out.println(Arrays.toString(data));
-		}catch(Exception ex){
-			System.out.println("ClientResponse: "+ex);
-			return 0;
-		}
-	}
-	
-	public static void getImage(DataInputStream in){
-		String fname="pokemon";
-		System.out.println("Entre a getImage");
-		byte[] data = new byte[1024];
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		//Cuantos datos envio el server
-		int read;
-		try{
-			while((read = in.read()) != -1) {
-				baos.write(read);
-			}
-		}catch(Exception e){ System.out.println(e);}
-		System.out.println(baos);
-		try (FileOutputStream fos = new FileOutputStream(fname)) {
-		   fos.write(baos.toByteArray());
-		   fos.close();
-		}catch(Exception e){
-			System.out.println("Cant write img "+e);
-		}try{
-			BufferedImage image = ImageIO.read(new File(fname));
-			JLabel label = new JLabel(new ImageIcon(image));
-			JFrame f = new JFrame();
-			f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			f.getContentPane().add(label);
-			f.pack();
-			f.setLocation(200,200);
-			f.setVisible(true);
-			// Chanfle similar a getchar en c
-			Scanner sc = new Scanner(System.in);
-			sc.nextLine();
-			/*
-			final Runtime rt = Runtime.getRuntime();
-			rt.exec("xdg-open pokemon");
-			*/
-			/*
-			if (Desktop.isDesktopSupported()) {
-				Desktop.getDesktop().open(new File(fname));
-			} 
-			*/
-		}catch(Exception ex){
-			System.out.println("Error at getImage "+ex);
-		}
-	}
-	
-	public static void iniciaConexion(DataOutputStream out){
-		try{
-			byte b = 10;
-			byte[] bytes = {b};
-			out.write(bytes);
-		}catch(Exception ex){
-			System.out.println("Ocurrio un error al enviar los datos");
-		}
-	}
-	
-	public static void enviaSi(DataOutputStream out){
-		try{
-			byte b = 30;
-			byte[] bytes = {b};
-			out.write(bytes);
-		}catch(Exception ex){
-			System.out.println("Ocurrio un error al enviar los datos");
-		}
-	}
-	
-	public static void enviaNo(DataOutputStream out){
-		try{
-			byte b = 31;
-			byte[] bytes = {b};
-			out.write(bytes);
-		}catch(Exception ex){
-			System.out.println("Ocurrio un error al enviar los datos");
-		}
-	}
-	
-	public static void terminaSesion(DataOutputStream out){
-		try{
-			System.out.println("Entre a termina sesion");
-			byte b = 32;
-			byte[] bytes = {b};
-			out.write(bytes);
-		}catch(Exception ex){
-			System.out.println("Ocurrio un error al enviar los datos");
-		}	
-	}
-	
-	public static void sendResponse(DataOutputStream out,Scanner sc){
-		String linea = sc.nextLine();
-		System.out.println("linea "+linea);
-		if(linea.equals("y")){
-			System.out.println("SI");
-			enviaSi(out);
-		}else{
-			System.out.println("NO");
-			enviaNo(out);
-		}
-		//parseResponse(in);
-	}
-	
-	public static void main(String [] args) {
-		String serverName = "127.0.0.1";
-		int port = Integer.parseInt("9999");
-		try {
-			// Inicio de conexion y edatos
-			System.out.println("Connecting to " + serverName + " on port " + port);
-			Socket client = new Socket(serverName, port);
-			Scanner sc = new Scanner(System.in);
-			// Buffers
-			InputStream inFromServer = client.getInputStream();
-			DataInputStream in = new DataInputStream(inFromServer);
-			System.out.println("Just connected to " + client.getRemoteSocketAddress());
-			OutputStream outToServer = client.getOutputStream();
-			DataOutputStream out = new DataOutputStream(outToServer);
-			// Inicio de conexion y edatos
-			iniciaConexion(out);
-			// Parse response
-			while(true){
-				if(parseResponse(in,out) == 1)
-					sendResponse(out,sc);
-			}
-			//client.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+		  }
+		  return;
+   }catch(Exception ex){System.out.println("Error"+ex);}
+}
+   
+   public static void main(String [] args) {
+      try{
+		  int port = Integer.parseInt("9999");
+		  ServerSocket serverSocket = new ServerSocket(port);
+		  //Socket server = null;
+		  while(true){
+				Socket server = serverSocket.accept();
+				System.out.println(server);
+				Thread t = new Server(server,port);
+				t.start();			  
+		  }
+      }catch (IOException ex) {
+         ex.printStackTrace();
+	  }
+   }
 }
